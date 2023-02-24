9919af40aa6656062ace5e2d0ee0a5663ae8f0ac
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-02-24 16:47:25 +0100

    Dadi

---
+++ dadi/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const playerNumber = Math.floor(Math.random() * 6 + 1);
+console.log(`Numero del giocatore = ${playerNumber}`);
+const computerNumber = Math.floor(Math.random() * 6 + 1);
+console.log(`Numero del computer = ${computerNumber}`);
+alert('Benvenuto a un gioco di dadi!');
+alert(`Il tuo numero è ${playerNumber}`);
+alert(`Il numero del computer è ${computerNumber}`);
+
+if (playerNumber > computerNumber) {
+    alert(`Congratulazioni, il tuo ${playerNumber} batte il ${computerNumber} del computer!`);
+    console.log('Il giocatore ha vinto la partita');
+} else if (computerNumber > playerNumber) {
+    alert(`Peccato! Il computer ha battuto il tuo ${playerNumber} con un ${computerNumber}.`);
+    console.log('Il computer ha vinto la partita');
+} else {
+    alert(`Ad entrambi è uscito un ${playerNumber}, abbiamo un pareggio!`);
+    console.log('Il giocatore e il computer hanno pareggiato')
+}
