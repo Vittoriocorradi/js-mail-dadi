52cbef90b463f1762f0586b6a1c0273fa462269c
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-02-24 16:53:16 +0100

    Commenti

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,13 +1,16 @@
 'use strict';
 
+// Variabili numeri casuali e rispettivi console.log
 const playerNumber = Math.floor(Math.random() * 6 + 1);
 console.log(`Numero del giocatore = ${playerNumber}`);
 const computerNumber = Math.floor(Math.random() * 6 + 1);
 console.log(`Numero del computer = ${computerNumber}`);
+// Alert di inizio partita
 alert('Benvenuto a un gioco di dadi!');
 alert(`Il tuo numero è ${playerNumber}`);
 alert(`Il numero del computer è ${computerNumber}`);
 
+// Condizione vincitore
 if (playerNumber > computerNumber) {
     alert(`Congratulazioni, il tuo ${playerNumber} batte il ${computerNumber} del computer!`);
     console.log('Il giocatore ha vinto la partita');
--- mail/js/main.js
+++ mail/js/main.js
@@ -1,17 +1,22 @@
 'use strict';
 
+// Variabili lista email e email dell'utente
 const mailList = ['pinco.pallino@mail.com', 'gianni.pinotto@mail.com', 'pippo.pluto@mail.com', 'qui.quo@mail.com'];
 console.log(mailList);
-let logIn = false;
 const userMail = prompt('Inserire il proprio indirizzo email');
 console.log(userMail);
+// Variabile per vedere se l'utente può accedere o no
+let logIn = false;
 
+// Ciclo per leggere gli elementi dell'array
 for (let i = 0; i < mailList.length; i++) {
+    // Condizione per controllare che la mail dell'utente sia nella lista
     if (mailList[i] === userMail) {
         logIn = true;
     }
 }
 
+// Condizione per determinare il risultato da dare all'utente
 if (logIn === true) {
     alert('Puoi accedere al sito');
     console.log(`L'utente può accedere al sito`);
